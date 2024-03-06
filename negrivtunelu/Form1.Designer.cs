d0c56087e75a21577d20c1783a9e4ce837c8e437
Matej <Matej@LAPTOP-69UR2M8T>
2024-03-06 17:36:17 +0100

    neg

--- negrivtunelu/Form1.Designer.cs
+++ negrivtunelu/Form1.Designer.cs
@@ -29,61 +29,73 @@
         private void InitializeComponent()
         {
             this.pbPlatno = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.ssBottom = new System.Windows.Forms.StatusStrip();
             this.tsMysSouradnice = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btSmazatPlatno = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
             this.ssBottom.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbPlatno
             // 
+            this.pbPlatno.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pbPlatno.BackColor = System.Drawing.Color.White;
             this.pbPlatno.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pbPlatno.Location = new System.Drawing.Point(12, 12);
+            this.pbPlatno.Location = new System.Drawing.Point(50, 46);
+            this.pbPlatno.Margin = new System.Windows.Forms.Padding(6);
             this.pbPlatno.Name = "pbPlatno";
-            this.pbPlatno.Size = new System.Drawing.Size(500, 304);
+            this.pbPlatno.Size = new System.Drawing.Size(1073, 522);
+            this.pbPlatno.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbPlatno.TabIndex = 0;
             this.pbPlatno.TabStop = false;
+            this.pbPlatno.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbPlatno_MouseDown);
             this.pbPlatno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbPlatno_MouseMove);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(601, 148);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 72);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.pbPlatno.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbPlatno_MouseUp);
             // 
             // ssBottom
             // 
+            this.ssBottom.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.ssBottom.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsMysSouradnice});
-            this.ssBottom.Location = new System.Drawing.Point(0, 428);
+            this.ssBottom.Location = new System.Drawing.Point(0, 823);
             this.ssBottom.Name = "ssBottom";
-            this.ssBottom.Size = new System.Drawing.Size(800, 22);
+            this.ssBottom.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.ssBottom.Size = new System.Drawing.Size(1600, 42);
             this.ssBottom.TabIndex = 2;
             this.ssBottom.Text = "statusStrip1";
             // 
             // tsMysSouradnice
             // 
             this.tsMysSouradnice.Name = "tsMysSouradnice";
-            this.tsMysSouradnice.Size = new System.Drawing.Size(46, 17);
+            this.tsMysSouradnice.Size = new System.Drawing.Size(94, 32);
             this.tsMysSouradnice.Text = "x: 0 y: 0";
+            // 
+            // btSmazatPlatno
+            // 
+            this.btSmazatPlatno.Location = new System.Drawing.Point(0, -5);
+            this.btSmazatPlatno.Name = "btSmazatPlatno";
+            this.btSmazatPlatno.Size = new System.Drawing.Size(54, 55);
+            this.btSmazatPlatno.TabIndex = 3;
+            this.btSmazatPlatno.Text = "C";
+            this.btSmazatPlatno.UseVisualStyleBackColor = true;
+            this.btSmazatPlatno.Click += new System.EventHandler(this.btSmazatPlatno_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.btSmazatPlatno);
             this.Controls.Add(this.ssBottom);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pbPlatno);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Kreslení";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
             this.ssBottom.ResumeLayout(false);
             this.ssBottom.PerformLayout();
@@ -95,9 +107,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pbPlatno;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.StatusStrip ssBottom;
         private System.Windows.Forms.ToolStripStatusLabel tsMysSouradnice;
+        private System.Windows.Forms.Button btSmazatPlatno;
     }
 }
 
--- negrivtunelu/Form1.cs
+++ negrivtunelu/Form1.cs
@@ -14,9 +14,15 @@ namespace negrivtunelu
     {
         //kreslici objekt grafiky
         Graphics mobjGrafika;
+        Point? predchoziBod = null;
+        bool Kresli = false;
+        
         public Form1()
         {
             InitializeComponent();
+           //umoznuje mazani pomoci c
+            this.KeyPreview = true;
+            
         }
         /// <summary>
         /// nahrani okna do pameti
@@ -28,12 +34,6 @@ namespace negrivtunelu
             //vytvoreni grafiky na pictureboxu
             mobjGrafika = pbPlatno.CreateGraphics();
         }
-        //testovaci tlacitko
-        private void button1_Click(object sender, EventArgs e)
-        {
-            mobjGrafika.DrawLine(Pens.Red, 10, 10, 100, 100);
-            mobjGrafika.FillEllipse(Brushes.RoyalBlue, 80,80,200,400);
-        }
         /// <summary>
         /// pohyb mysi na picture boxem
         /// </summary>
@@ -43,13 +43,66 @@ namespace negrivtunelu
         {
             try 
             {
-            //souradnice mysi do statusu
-            tsMysSouradnice.Text="x:" + e.X.ToString() + "y:" + e.Y.ToString();
+                //souradnice mysi do statusu
+                tsMysSouradnice.Text ="x:" + e.X.ToString() + "y:" + e.Y.ToString();
+                //kontrola jestli mousedown
+                if(Kresli== true ) 
+                {   
+                    if (predchoziBod != null) 
+                    {
+                        mobjGrafika.DrawLine(Pens.Black, (Point)predchoziBod, new Point(e.X, e.Y));
+                    }
+                   // ulozi aktualni coords do predchoziBod
+                    predchoziBod = new Point(e.X, e.Y);
+                }
             }
             catch(Exception ex) 
             {
             
             }
         }
+        //
+        //stisknuti tlacitka mysi pro kresleni
+        //
+        private void pbPlatno_MouseDown(object sender, MouseEventArgs e)
+        {
+        Kresli = true;
+        predchoziBod = new Point(e.X, e.Y);
+        }
+        //
+        //zvednuti tlacitka mysi pri kresleni
+        //
+        private void pbPlatno_MouseUp(object sender, MouseEventArgs e)
+        {
+            Kresli= false;
+        }
+        //
+        //rutina pro vymazani platna
+        //
+        private void btSmazatPlatno_Click(object sender, EventArgs e)
+        {
+            mobjGrafika.Clear(Color.White);
+        }
+        // 
+        //rutina pro mazani stisknutim c
+        //
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C) 
+            {
+            btSmazatPlatno_Click(sender, e);
+            }
+        }
+        // 
+        //rutina pro rozsireni kreslici zony pri zvetseni okna
+        //
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+           
+            mobjGrafika = pbPlatno.CreateGraphics();
+            
+        }
+        
     }
+
 }
