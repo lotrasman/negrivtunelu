6403c8d18c1aade40b554f81e0f53778f42a1233
Matej <Matej@LAPTOP-69UR2M8T>
2024-03-19 12:55:25 +0100

    negr default barva

--- negrivtunelu/Form1.cs
+++ negrivtunelu/Form1.cs
@@ -24,6 +24,7 @@ namespace negrivtunelu
             InitializeComponent();
            //umoznuje mazani pomoci c
             this.KeyPreview = true;
+            mobjAktBarva = Color.Black;
             
         }
         /// <summary>
